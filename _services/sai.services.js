14d1788d351792f5ab8ed0222b130951c0d2c016
Mike Alvarez <mike@allcode.com>
2020-05-11 16:18:22 -0500

    added request

--- _services/sai.services.js
+++ _services/sai.services.js
@@ -7,7 +7,7 @@ function getAllSubjects() {
     const requestOptions = {
         method: 'GET',
     };
-    return fetch('http://localhost:8080/api/materia/all' ,requestOptions).then(handleResponse)
+    return fetch('http://192.168.100.3:8080/api/materia/all' ,requestOptions).then(handleResponse)
 }
 
 function getGroupsBySubject(id) {
@@ -15,10 +15,11 @@ function getGroupsBySubject(id) {
         method: 'GET',
     };
 
-    return fetch('http://localhost:8080/api/materia/'+ id +'/grupos' ,requestOptions).then(handleResponse)
+    return fetch('http://192.168.100.3:8080/api/materia/'+ id +'/grupos' ,requestOptions).then(handleResponse)
 }
 
 function handleResponse(response) {
+    console.log("response", response);
     if(response && response.status && response.status === 404) {
         return response;
     }
