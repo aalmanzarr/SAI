2fcec36ef3a4e6adb1ccf25485ab0fe29fdaed70
unknown <raavendanp@eafit.edu.co>
2020-05-11 23:38:27 -0500

    ruteo

--- App.js
+++ App.js
@@ -11,66 +11,24 @@ import Profile from './components/screens/Profile';
 import Settings from './components/screens/Settings';
 import Main from './components/screens/Main';
 import Login from './components/screens/Login';
+import Dashboard from './components/screens/Dashboard';
 
 const Tab = createMaterialBottomTabNavigator();
 const Stack = createStackNavigator();
 export default function App() {
   return (
    
-  
     <NavigationContainer>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignSelf: 'flex-start',
-          width: '100%',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: 70,
-          backgroundColor: '#000068',
-        }}>
-        <Icon name="university" size={25} color="#ffffff" />
-        <Text style={{color: 'white', fontSize: 22, marginLeft: 5}}>EAFIT</Text>
-      </View>
-      <Tab.Navigator
-        initialRouteName="Login"
-        activeColor="#FFFFFF"
-        inactiveColor="#FFFFFF"
-        barStyle={{backgroundColor: '#000068'}}
-        labeled={false}>
-        <Tab.Screen
-          name="Settings"
-          component={Settings}
-          options={{
-            tabBarLabel: 'Ajustes',
-            tabBarIcon: () => <Icon name="cog" size={25} color={'#ffffff'} />,
-          }}
-        />
-         <Tab.Screen
-          name="Login"
-          component={Login}
-          options={{
-            tabBarLabel: 'Inicio',
-            tabBarIcon: () => <Icon name="home" size={25} color={'#ffffff'} />,
-          }}
-        />
-        <Tab.Screen
-          name="Main"
-          component={Main}
-          options={{
-            tabBarLabel: 'Inicio',
-            tabBarIcon: () => <Icon name="home" size={25} color={'#ffffff'} />,
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            tabBarLabel: 'Perfil',
-            tabBarIcon: () => <Icon name="user" size={25} color="#ffffff" />,
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+    screenOptions={{
+                    headerShown: false
+                }}>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+      />
+      <Stack.Screen name="Main" component={Dashboard} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
